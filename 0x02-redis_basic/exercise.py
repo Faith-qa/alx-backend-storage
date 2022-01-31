cb0fd5944bd64822485474426de37c71a5262d54
Faith-qa <atienofaith12@gmail.com>
2022-01-31 17:33:09 +0300

    changes

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -6,38 +6,31 @@ import uuid
 from typing import Callable, Optional, Union
 class Cache:
     def __init__(self):
-        """stores an instance of reddis"""
+        """construct the instance"""
         self._redis = redis.Redis()
-
-        """flush the instance"""
         self._redis.flushdb()
 
+    
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        """takes data argument and returns a string"""
+        """ store new data and return a new uuid"""
         key = str(uuid.uuid4())
         self._redis.mset({key: data})
         return key
 
-
-    def get(self, key:str, fn: Optional[Callable] = None) -> Union[str, int, float, bytes]:
-        """
-        takes a key: argument and an optional 
-        calable argument called fn: fn will be used to 
-        convert data back to the desired type
-        """
+    def get(self,
+            key: str,
+            fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        """ get the element and return the decoded version"""
         data = self._redis.get(key)
-        if fn is not None:
+        if (fn is not None):
             return fn(data)
         return data
-       
-    def get_str(self, data:str) -> str:
-        """
-        this method will automatically parametirize catche.get
-        to the correct conversion function
-        """
-        return data.decode('utf-8')  
-    def get_int(self, data: int) -> int:
-        """
-        that will automatically parametrize Cache.get with the correct conversion function.
-        """
+
+    def get_str(self, data: str) -> str:
+        """ return the decoded byte in string """
+        return data.decode('utf-8')
+
+    def get_int(self, data: str) -> int:
+        """ return the decoded byte in integer """
         return int(data)
+    
